9af48b42cd21b5ae2e31cb10e3b522f15157b8cf
f <f@Hp-Fede.lan>
2018-11-20 18:19:13 +0100

    base

---
+++ src/it/polito/tdp/carsharing/Simulatore.java
@@ -0,0 +1,5 @@
+package it.polito.tdp.carsharing;
+
+public class Simulatore {
+
+}
---
+++ src/it/polito/tdp/carsharing/TestSimulatore.java
@@ -0,0 +1,10 @@
+package it.polito.tdp.carsharing;
+
+public class TestSimulatore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
